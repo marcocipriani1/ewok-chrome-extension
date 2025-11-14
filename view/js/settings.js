e0742a97f0d9c505dc09a35931e4157133dd15ff
Marco Cipriani <marcocipriani@tutanota.com>
2025-11-15 00:19:11 +0100

    add license headers

--- view/js/settings.js
+++ view/js/settings.js
@@ -1,3 +1,19 @@
+/*
+Ewok Chrome Extension: integrates with ewokd and the bot to track EWOQ tasks, forward alerts, and generate work-time reports.
+Copyright (C) 2023-2025 Marco Cipriani <marcocipriani@tutanota.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/.
+*/
+
 import {logAllData, calculateRph, formatTime} from "./utils.js";
 
 let exportCSVBtn = document.getElementById("export-csv-btn");
